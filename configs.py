b61a76d90ef51fa5b47818160d84952de382a841
Ajay20217 <106938620+Ajay20217@users.noreply.github.com>
2024-06-05 18:58:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "7023173279:AAF8YCrCbZH6Z2hhwVZjIe945YuUpC-mzKA")
     FSUB = getenv("FSUB", "0")
     CHID = int(getenv("CHID", "-1001939184003"))
-    SUDO = list(map(int, getenv("6036497605").split()))
+    SUDO = list(map(int, getenv("SUDO","6036497605").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://naibansari987:Yuuichi@cluster0.vbota1p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
